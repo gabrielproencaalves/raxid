e1a4c79c8c65ba8ca4ca5c0d2e455a787d3c89ee
behemot <behemot@tutamail.com>
2025-05-17 16:32:09 -0300

    ref:  Trocado path padrao para /raixd.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import fetchPage from "./components/fetchPage.js";
 
 const routes = {
-  "/": { file: "./view/home.html", element: "main", script: ["./js/home.js"] },
+  "/raixd": { file: "./view/home.html", element: "main", script: ["./js/home.js"] },
   "/register": { file: "./view/register.html", element: "main", script: ["./js/register.js"] },
   "/analistic": { file: "./view/analistic.html", element: "main", script: ["./js/analistic.js"] }
 };
