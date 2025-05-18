d9463eba5fc74217f346c9a7492b82409a41aa13
behemot <behemot@tutamail.com>
2025-05-18 02:12:07 -0300

    ref: Troca de manipulacoa do aplicativo para modal

--- js/home.js
+++ js/home.js
@@ -1,16 +1,3 @@
-import router from "./router.js";
-
 $(document).ready(function(e){
-	
-	const url = window.location.origin;
 
-	$("#btn-register").on("click", function(e){
-		e.preventDefault();
-		router('/register')
-	});
-
-	$("#btn-consult").on("click", function(e){
-		e.preventDefault();
-		window.location.replace(url + "/analistic");
-	});
 });
